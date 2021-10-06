20493cb37637e1d876bc5c612d7d75c5f60473e8
Baki OZTURK <22.exe.94@gmail.com>
2021-10-06 22:29:46 +0300

    Add project files.

---
+++ MyBIP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyBIP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
